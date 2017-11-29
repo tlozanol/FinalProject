4c578b6b3ec2a44d495b55f34a2c504e28cfba72
Nathalie Beaudry <beau0737@algonquinlive.com>
2017-11-29 12:08:03 -0500

    Added FoodTracker.

--- app/src/main/java/team/best/team/finalproject/FoodTrackerActivity.java
+++ app/src/main/java/team/best/team/finalproject/FoodTrackerActivity.java
@@ -2,12 +2,26 @@ package team.best.team.finalproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class FoodTrackerActivity extends Activity {
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_tracker);
+
+        Button btnFoodTracker = (Button) findViewById(R.id.btnFoodTrackerEnter);
+        EditText editTextFoodTracker = (EditText) findViewById(R.id.editTextFoodTracker);
+        ListView ListFood = (ListView) findViewById(R.id.listFood);
+        String[] ItemMenu = {"Apple", "Banana", "Carrot", "Celeri", "Grapes", "Mango", "Orange"};
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ItemMenu);
+        ListFood.setAdapter(listViewAdapter);
+
+
     }
 }
