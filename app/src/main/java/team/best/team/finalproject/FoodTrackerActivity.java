a6b4372f5053ca77be727a59863c0a2b51725a3e
Nathalie Beaudry <beau0737@algonquinlive.com>
2017-12-03 14:53:15 -0500

    Added activities linked to listview of food. New activities contain nutritional info for apple and banana.

--- app/src/main/java/team/best/team/finalproject/FoodTrackerActivity.java
+++ app/src/main/java/team/best/team/finalproject/FoodTrackerActivity.java
@@ -1,7 +1,11 @@
 package team.best.team.finalproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,11 +20,43 @@ public class FoodTrackerActivity extends Activity {
 
         Button btnFoodTracker = (Button) findViewById(R.id.btnFoodTrackerEnter);
         EditText editTextFoodTracker = (EditText) findViewById(R.id.editTextFoodTracker);
-        ListView ListFood = (ListView) findViewById(R.id.listFood);
-        String[] ItemMenu = {"Apple", "Banana", "Carrot", "Celeri", "Grapes", "Mango", "Orange"};
+        final ListView ListFood = (ListView) findViewById(R.id.listFood);
+        String[] ItemMenu = {"Apple", "Banana"};
 
         ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ItemMenu);
         ListFood.setAdapter(listViewAdapter);
+
+        ListFood.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+
+                if(position==0){
+                    Intent intent = new Intent(view.getContext(), FoodTrAppleInfoActivity.class);
+                    startActivity(intent);
+                }
+
+                if(position==1){
+                    Intent intent = new Intent(view.getContext(), FoodTrBananaInfoActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+
+
+        //Button buttonStartListItems = findViewById(R.id.button);
+       // buttonStartListItems.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v){
+        //        Log.i(ACTIVITY_NAME, "User clicked Start List Items");
+        //        Intent intent = new Intent(StartActivity.this, ListItemsActivity.class);
+        //       startActivityForResult(intent, 10);
+         //   }
+        //});
+
 
 
     }
