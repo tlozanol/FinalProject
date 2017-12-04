59e61b4d510834a780a54ab4b6e7a53d06bc93b5
Tlozano <loza0025@algonquinlive.com>
2017-12-04 14:47:47 -0500

    branch1

--- app/src/main/java/team/best/team/finalproject/AutomobileActivity.java
+++ app/src/main/java/team/best/team/finalproject/AutomobileActivity.java
@@ -2,12 +2,20 @@ package team.best.team.finalproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AutomobileActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME = "AutomobileActivity";
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_automobile);
     }
+
+    Button seeHistoryDB = findViewById(R.id.seeHistory);
+    
+
 }
