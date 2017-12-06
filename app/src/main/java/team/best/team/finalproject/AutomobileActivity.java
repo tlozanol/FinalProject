569524e0fa9c6ba2e79b48b5b8955732d7452ebd
Tlozano <loza0025@algonquinlive.com>
2017-12-05 22:48:16 -0500

    branch1
    Milestone 1

--- app/src/main/java/team/best/team/finalproject/AutomobileActivity.java
+++ app/src/main/java/team/best/team/finalproject/AutomobileActivity.java
@@ -1,7 +1,9 @@
 package team.best.team.finalproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,7 +20,9 @@ public class AutomobileActivity extends Activity {
         seeHistoryDB.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                
+                Log.i(ACTIVITY_NAME, "-- User clicked History");
+                Intent intent = new Intent(AutomobileActivity.this, Automobile_History.class);
+                startActivity(intent);
             }
         });
     }
