261d816bdb673de67a7b32b52b7cdf0483270164
Caty <34069595+catyjane@users.noreply.github.com>
2017-12-06 10:44:12 -0500

    Adding ActivityTracker for Milestone1

---
+++ app/src/main/java/team/best/team/finalproject/MyEditTextDatePicker.java
@@ -0,0 +1,11 @@
+package team.best.team.finalproject;
+
+import android.view.View;
+
+/**
+ * Created by Caty on 2017-12-05.
+ */
+
+public class MyEditTextDatePicker implements View.OnClickListener
+{
+}
