da785c7a0ea64743a21ec65a879dcbde09921467
Nathalie Beaudry <beau0737@algonquinlive.com>
2018-01-02 13:11:47 -0500

    Database connected and listview refreshes

--- app/src/main/java/team/best/team/finalproject/FoodTrackerItemFragment.java
+++ app/src/main/java/team/best/team/finalproject/FoodTrackerItemFragment.java
@@ -16,15 +16,25 @@ import android.widget.TextView;
 
 public class FoodTrackerItemFragment extends Fragment {
     
-    private Button FTbtnQuit;
+    //private Button FTbtnQuit;
     
     protected static final String ACTIVITY_NAME = "FoodTrackerItemFragment";
-   
+    private final int FOODTRACKER_RESULT_SAVE = 100;
+    private final int FOODTRACKER_RESULT_DELETE = 200;
+    private FoodTrackerActivity foodtrackerActivity;
+    private FoodTrackerItemDetails foodTrackerItemDetails;
+    
+    private int clickedItemID;
+    private EditText nameItem;
+    private EditText calValue;
+    private EditText fatValue;
+    private EditText carbsValue;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(ACTIVITY_NAME, "-- In onCreate");
-    
+        
         //setContentView(R.layout.activity_food_tracker_item_fragment);
     }
     
@@ -32,27 +42,80 @@ public class FoodTrackerItemFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         Log.i(ACTIVITY_NAME, "-- In onCreateView");
-    
+        
         View view = inflater.inflate(R.layout.activity_food_tracker_item_fragment, null);
-        //View view = inflater.inflate(R.layout.activity_food_tracker_item_fragment,  null);
-       // TextView FTFragLabel = view.findViewById(R.id.FTFragLabel);
-       // FTFragLabel.setText("APPLE");
-      
-        //////  ADD SNACKBAR TO SAVE BUTTON
-          Button FTbtnSaveMess = view.findViewById(R.id.FTbtnSaveMess);
-          FTbtnSaveMess.setOnClickListener(new View.OnClickListener() {
-              @Override
-              public void onClick(View view) {
-                  Snackbar.make(view , "Your Food Item has been saved!", Snackbar.LENGTH_SHORT).show();
-              }
-          });
+        Log.i(ACTIVITY_NAME, "-- In onCreateView, after view is created");
+        Bundle foodtrackerEntryBundle = getArguments();
+        boolean inAdd = foodtrackerEntryBundle.getBoolean("InAdd", false);
+        clickedItemID = foodtrackerEntryBundle.getInt("ID", -1);
+        String clickedName = foodtrackerEntryBundle.getString("Name", "");
+        String clickedCal = foodtrackerEntryBundle.getString("Cal", "");
+        String clickedFat = foodtrackerEntryBundle.getString("Fat", "");
+        String clickedCarbs = foodtrackerEntryBundle.getString("Carbs", "");
         
-    
-    
         
-     
+        nameItem = view.findViewById(R.id.FTeditTxtItemName);
+        nameItem.setText(clickedName);
+        
+        calValue = view.findViewById(R.id.FTeditTxtCalories);
+        calValue.setText(clickedCal);
+        
+        fatValue = view.findViewById(R.id.FTeditTxtTotalFat);
+        fatValue.setText(clickedFat);
+        
+        carbsValue = view.findViewById(R.id.FTeditTxtTotalCarbs);
+        carbsValue.setText(clickedCarbs);
+        
+        //// SAVE BUTTON
+        Log.i(ACTIVITY_NAME, "-- In onCreate, before click SAVE");
+        Button btnSaveFoodEntry = view.findViewById(R.id.FTbtnSaveMess);
+        btnSaveFoodEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+    
+                Intent saveEntryIntent = createFoodTrackerEntryIntent();
+                foodTrackerItemDetails.setResult(FOODTRACKER_RESULT_SAVE, saveEntryIntent);
+                foodTrackerItemDetails.finish();
+                
+            }
+        });
+        Log.i(ACTIVITY_NAME, "-- In onCreate, AFTER click SAVE");
+        //// DELETE BUTTON
+        Button btnDeleteFoodEntry = view.findViewById(R.id.FTbtnDeleteMesg);
+        btnDeleteFoodEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+                Intent deleteEntryIntent = createFoodTrackerEntryIntent();
+                foodTrackerItemDetails.setResult(FOODTRACKER_RESULT_DELETE, deleteEntryIntent);
+                foodTrackerItemDetails.finish();
+                
+            }
+        });
+        
         return view;
     }
-
     
+    public void setParent(FoodTrackerItemDetails parent) {
+        Log.i(ACTIVITY_NAME, "-- In setParent");
+        foodTrackerItemDetails = parent;
+    }
+    
+    private Intent createFoodTrackerEntryIntent() {
+        
+        Intent resultIntent = new Intent();
+        
+        resultIntent.putExtra("ID", clickedItemID);
+        
+        ///// ADD limits here
+        resultIntent.putExtra("Name", nameItem.getText().toString());
+        
+        resultIntent.putExtra("Cal", calValue.getText().toString());
+        resultIntent.putExtra("Fat", fatValue.getText().toString());
+        resultIntent.putExtra("Carbs", carbsValue.getText().toString());
+        Log.i(ACTIVITY_NAME, "-- In createFoodTrackerEntryIntent");
+        
+        
+        return resultIntent;
+    }
 }
